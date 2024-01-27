72c41cb3f08f7a6a39f0538610dd15d6428d92c5
Polina Lepekhina <wdvrgnyjio6@gmail.com>
2024-01-27 20:21:18 +0300

    mesto-project-ff

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placeslist = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+initialCards.forEach (function (element){
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__title').textContent = element.name;
+
+    placeslist.append(cardElement);
+
+    const deleteButton = document.querySelector('.card__delete-button');
+});
+
 
 // @todo: Функция удаления карточки
 
+deleteButton.addEventListener('click', function () {
+    // ???
+  }); 
 // @todo: Вывести карточки на страницу
