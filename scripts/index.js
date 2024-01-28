b74eb9d4e730e096f8f1811c799bcdd90639a1e3
Polina Lepekhina <wdvrgnyjio6@gmail.com>
2024-01-28 16:48:17 +0300

    mesto-project-ff

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ function createCard(element, deleteItem) {
   const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
 
   cardElement.querySelector(".card__image").src = element.link;
+  cardElement.querySelector(".card__image").alt = element.name;
   cardElement.querySelector(".card__title").textContent = element.name;
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
